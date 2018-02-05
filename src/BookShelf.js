76d84a5948b0812fd278d1cb63d0d38cdc31ae5a
wightman <davidwightmanmusic@gmail.com>
2018-02-04 23:20:39 -0500

    refactor: broke project up into seperate components ListBook, BookShelf, and Book

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
 import Book from './Book';
 
+class BookShelf extends Component {
+	render() {
+		return (
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">Currently Reading</h2>
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+						{this.props.books.map((book, index) => <Book book={book} key={index} />)}
+					</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
 export default BookShelf;
--- src/ListBook.js
+++ src/ListBook.js
@@ -1,4 +1,34 @@
 import React, { Component } from 'react';
 import BookShelf from './BookShelf.js';
 
+class ListBook extends Component {
+	render() {
+		return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				<div className="list-books-content">
+					<div>
+						<BookShelf
+							books={this.props.books.filter(book => book.shelf === 'currentlyReading')}
+							title="Currently Reading"
+						/>
+
+						<BookShelf books={this.props.books.filter(book => book.shelf === 'read')} title="Read" />
+
+						<BookShelf
+							books={this.props.books.filter(book => book.shelf === 'wantToRead')}
+							title="Want to Read"
+						/>
+					</div>
+				</div>
+				<div className="open-search">
+					<a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+				</div>
+			</div>
+		);
+	}
+}
+
 export default ListBook;
